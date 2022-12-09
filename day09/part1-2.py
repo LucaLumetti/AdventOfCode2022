600d0dc70fdc879e6fcbcd4c091c1f8f80fd4145
Luca Lumetti <lumetti.luca@gmail.com>
2022-12-09 13:35:48 +0100

    improv day09

--- day09/part1-2.py
+++ day09/part1-2.py
@@ -1,15 +1,6 @@
 from math import sqrt, atan2, cos, sin, pi
 
-def move(R, d):
-    if d == 'R':
-        R[0] += 1
-    if d == 'L':
-        R[0] -= 1
-    if d == 'U':
-        R[1] += 1
-    if d == 'D':
-        R[1] -= 1
-    return R
+D = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
 
 def step(H, T):
     dist = sqrt((H[0]-T[0])**2 + (H[1]-T[1])**2)
@@ -31,11 +22,10 @@ with open('input') as f:
 R = [[0, 0] for _ in range(10)]
 history = [{str(R[1])},{str(R[-1])}]
 
-for m in moves:
-    d, n = m
-
+for d, n in moves:
     for _ in range(n):
-        R[0] = move(R[0], d)
+        R[0][0] += D[d][0]
+        R[0][1] += D[d][1]
         for t in range(1, 10):
             R[t-1], R[t] = step(R[t-1], R[t])
         history[0].add(f'{R[1]}')
