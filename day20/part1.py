c843808c310fca807973ddc0ada5f95c2f528da9
Luca Lumetti <lumetti.luca@gmail.com>
2022-12-20 21:12:55 +0100

    day20

---
+++ day20/part1.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+x = list(map(int, open('input').read().splitlines()))
+l = len(x)
+v_i = deque([(v, i) for i, v in enumerate(x)])
+
+for i,n in enumerate(x):
+    idx = v_i.index((n, i))
+    v_i.remove((n, i))
+    v_i.insert((idx+n)%(l-1), (n, i))
+
+s = [vi[0] for vi in v_i]
+zero = s.index(0)
+print(s[(zero+1000) % l]+s[(zero+2000) % l]+s[(zero+3000) % l])
